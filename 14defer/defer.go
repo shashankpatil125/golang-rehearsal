a190f8ed5d1797717dda094698e922676d1f306a
shashankpatil125 <shashankpatil125@gmail.com>
2023-03-09 06:17:26 +0530

    defer

---
+++ 14defer/defer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func check() int {
+	defer fmt.Println("one")
+	fmt.Println("two")
+	return 3
+}
+
+func main() {
+	fmt.Println(check())
+}
+
+//it is used such cases as
+// 1 resousces cleanup
+// 2 close file
+// 3 debugging or testing on database
